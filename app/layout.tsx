e407a819b7a7fad95953defee18d87adad4793a0
Ayush negi <87976275+yuyuayush@users.noreply.github.com>
2024-12-11 00:28:22 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import {Poppins} from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: "--font-poppins",
+})
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Card.tsx
@@ -0,0 +1,41 @@
+import { Models } from 'node-appwrite'
+import React from 'react'
+import FormattedDateTime from './FormattedDateTime'
+import ActionDropdown from './ActionDropdown'
+import { convertFileSize } from '@/lib/utils'
+import Link from 'next/link'
+import Thumbnail from './Thumbnail'
+
+const Card = ({file}:{file:Models.Document}) => {
+  return (
+    <Link href={file.url} target="_blank" className="file-card">
+    <div className="flex justify-between">
+      <Thumbnail
+        type={file.type}
+        extension={file.extension}
+        url={file.url}
+        className="!size-20"
+        imageClassName="!size-11"
+      />
+
+      <div className="flex flex-col items-end justify-between">
+        <ActionDropdown file={file} />
+        <p className="body-1">{convertFileSize(file.size)}</p>
+      </div>
+    </div>
+
+    <div className="file-card-details">
+      <p className="subtitle-2 line-clamp-1">{file.name}</p>
+      <FormattedDateTime
+        date={file.$createdAt}
+        className="body-2 text-light-100"
+      />
+      <p className="caption line-clamp-1 text-light-200">
+        By: {file.owner.fullName}
+      </p>
+    </div>
+  </Link>
+  )
+}
+
+export default Card
